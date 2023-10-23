1bf814551bd9b1eb0b30874c3802ef2db3718778
ZZOOONII <gks3380@gmail.com>
2023-10-23 18:53:24 +0900

    docs: 폴더구조 변경

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,45 @@ import Header from './components/Header'; // 헤더 컴포넌트
 import Footer from './components/Footer'; // 푸터 컴포넌트
 import './App.css';
 import Section from './components/Section';
-// import bg01 from "./bg01.png";
+// import section01_img from './section01-1.png';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className="content">
-      {/* <img src={bg01} alt="background_img"/> */}
+
+      <main className="mainContent" backgroundColor="black">
+        <div className="light">
+          <img src="/section01-1.png" alt="blacklight_img" />
+        </div>
+        <div className="small-rect1">
+          <img src="/section01-2.png" alt="section01-2" />
+        </div>
+        <div className="small-rect2">
+          <img src="/section01-3.png" alt="section01-3" />
+        </div>
       </main>
 
-      <Section backgroundColor="black">1번 섹션</Section>
-      <Section backgroundColor="gray">2번 섹션</Section>
+      <Section className="mainSection02" backgroundColor="#333333">
+        <img
+          src="/section02-1.png"
+          alt="section02_img"
+          style={{ width: '50%', height: 'auto' }}
+        />
+      </Section>
+
       <Section backgroundColor="black">3번 섹션</Section>
       <Section backgroundColor="white">4번 섹션</Section>
       <Section backgroundColor="black">5번 섹션</Section>
       <Section backgroundColor="white">6번 섹션</Section>
+      {/*  */}
+      <Section backgroundColor="white">
+        <Page1 />
+      </Section>
+      <Section backgroundColor="white">
+        <Page2 />
+      </Section>
+
       <Footer />
     </div>
   );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,9 +9,15 @@ function Header() {
       </div>
       <nav className="menu">
         <ul>
-          <li><a href="/">메뉴1</a></li>
-          <li><a href="/">메뉴2</a></li>
-          <li><a href="/">메뉴3</a></li>
+          <li>
+            <a href="/">메뉴1</a>
+          </li>
+          <li>
+            <a href="/">메뉴2</a>
+          </li>
+          <li>
+            <a href="/">메뉴3</a>
+          </li>
         </ul>
       </nav>
     </header>
@@ -19,3 +25,39 @@ function Header() {
 }
 
 export default Header;
+
+// 부트스트랩 헤더
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// function Header() {
+//   return (
+//     <Navbar expand="lg" className="bg-body-tertiary">
+//       <Container>
+//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="#home">Home</Nav.Link>
+//             <Nav.Link href="#link">Link</Nav.Link>
+//             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.2">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action/3.4">
+//                 Separated link
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+// export default Header;
